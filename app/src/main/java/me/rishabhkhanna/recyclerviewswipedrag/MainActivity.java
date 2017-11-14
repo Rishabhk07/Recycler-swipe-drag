ab84705c60d9bef95faca36bfd73c9f3a30a28c9
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-11-14 15:54:21 +0530

    recycler view setup
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/me/rishabhkhanna/recyclerviewswipedrag/MainActivity.java
+++ app/src/main/java/me/rishabhkhanna/recyclerviewswipedrag/MainActivity.java
@@ -2,12 +2,35 @@ package me.rishabhkhanna.recyclerviewswipedrag;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    ArrayList<String> dataArrayList;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        dataArrayList = getData();
+        recyclerView.setAdapter(new RecyclerAdapter(dataArrayList,this));
+    }
+
+    public static ArrayList<String> getData(){
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Iron Man");
+        arrayList.add("Batman");
+        arrayList.add("Super Man");
+        arrayList.add("Captain America");
+        arrayList.add("Thor : God of Thunder");
+        arrayList.add("Hulk");
+        arrayList.add("Wonder Woman");
+
+        return arrayList;
     }
 }
