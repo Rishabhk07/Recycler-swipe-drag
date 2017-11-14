ce265d51e143c41c67917997ba8dab313004a3f1
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-11-14 15:53:34 +0530

    adapter in kotlin
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

---
+++ app/src/main/java/me/rishabhkhanna/recyclerviewswipedrag/RecyclerAdapter.kt
@@ -0,0 +1,33 @@
+package me.rishabhkhanna.recyclerviewswipedrag
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import java.util.ArrayList
+
+/**
+ * Created by rishabhkhanna on 14/11/17.
+ */
+class RecyclerAdapter(internal var arrayList: ArrayList<String>, internal var context: Context) : RecyclerView.Adapter<RecyclerAdapter.Holder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
+        val li = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        return Holder(li.inflate(R.layout.item_layout, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: Holder, position: Int) {
+        holder.tvName.setText(arrayList.get(position))
+    }
+
+    override fun getItemCount(): Int {
+        return arrayList.size
+    }
+
+    inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val tvName: TextView= itemView.findViewById(R.id.tvName)
+    }
+}
