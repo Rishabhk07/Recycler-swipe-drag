996c16a052658a3918a36f04298b9593b5feb87d
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-11-15 12:21:01 +0530

    lib attached to recycler view
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/me/rishabhkhanna/recyclerviewswipedrag/MainActivity.java
+++ app/src/main/java/me/rishabhkhanna/recyclerviewswipedrag/MainActivity.java
@@ -4,21 +4,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
+
+import me.rishabhkhanna.recyclerswipedrag.RecyclerHelper;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recyclerView;
-    ArrayList<String> dataArrayList;
+    ArrayList<String> dataArrayList = new ArrayList<>();
+    public static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        dataArrayList = getData();
-        recyclerView.setAdapter(new RecyclerAdapter(dataArrayList,this));
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerAdapter recyclerAdapter = new RecyclerAdapter(dataArrayList,this);
+        recyclerView.setAdapter(recyclerAdapter);
+        ItemTouchHelper.Callback touchHelper = new RecyclerHelper(recyclerAdapter);
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(touchHelper);
+        itemTouchHelper.attachToRecyclerView(recyclerView);
+        dataArrayList.addAll(getData());
+        recyclerAdapter.notifyDataSetChanged();
     }
 
     public static ArrayList<String> getData(){
@@ -30,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
         arrayList.add("Thor : God of Thunder");
         arrayList.add("Hulk");
         arrayList.add("Wonder Woman");
-
         return arrayList;
     }
 }
