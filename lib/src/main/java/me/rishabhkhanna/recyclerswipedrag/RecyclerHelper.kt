1d2685ee47a71a69f98f0db0e939a3fb2a8e6eca
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-11-15 16:02:55 +0530

    helper implementing HelperAdapter
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- lib/src/main/java/me/rishabhkhanna/recyclerswipedrag/RecyclerHelper.kt
+++ lib/src/main/java/me/rishabhkhanna/recyclerswipedrag/RecyclerHelper.kt
@@ -2,29 +2,51 @@ package me.rishabhkhanna.recyclerswipedrag
 
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.helper.ItemTouchHelper
+import android.util.Log
+import java.util.*
 
 /**
  * Created by rishabhkhanna on 14/11/17.
  */
-class RecyclerHelper(var mAdapter : ItemTouchHelperAdapter) : ItemTouchHelper.Callback() {
+class RecyclerHelper(var list: ArrayList<String>,var mAdapter: RecyclerView.Adapter<RecyclerView.ViewHolder>) : ItemTouchHelper.Callback(), ItemTouchHelperAdapter {
+    override fun onMoved(fromPos: Int, toPos: Int) {
+        Log.d("TAG","From: " + fromPos)
+        Log.d("TAG","To: " + toPos)
+        list.removeAt(toPos)
+        mAdapter.notifyItemRemoved(toPos)
+    }
+
+    override fun onItemMoved(fromPosition: Int, toPosition: Int) {
+        if (fromPosition < toPosition) {
+            for (i in fromPosition until toPosition) {
+                Collections.swap(list, i, i + 1)
+            }
+        } else {
+            for (i in fromPosition downTo  toPosition + 1){
+                Collections.swap(list,i, i-1);
+            }
+        }
+        mAdapter.notifyItemMoved(fromPosition,toPosition)
+    }
+
     override fun getMovementFlags(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?): Int {
-        var dragFlags:Int = ItemTouchHelper.UP or ItemTouchHelper.DOWN or ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT;
-        var swipeFlags:Int = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT;
-        return makeMovementFlags(dragFlags,swipeFlags);
+        var dragFlags: Int = ItemTouchHelper.UP or ItemTouchHelper.DOWN or ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT;
+        var swipeFlags: Int = ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT;
+        return makeMovementFlags(dragFlags, swipeFlags);
     }
 
     override fun onMove(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, target: RecyclerView.ViewHolder?): Boolean {
-        mAdapter.onItemMoved(viewHolder!!.adapterPosition, target!!.adapterPosition)
+        onItemMoved(viewHolder!!.adapterPosition, target!!.adapterPosition)
         return true;
     }
 
     override fun onMoved(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, fromPos: Int, target: RecyclerView.ViewHolder?, toPos: Int, x: Int, y: Int) {
-        mAdapter.onItemMoved(fromPos,toPos)
+        onItemMoved(fromPos, toPos)
         super.onMoved(recyclerView, viewHolder, fromPos, target, toPos, x, y)
     }
 
     override fun onSwiped(viewHolder: RecyclerView.ViewHolder?, direction: Int) {
-        mAdapter.onMoved(viewHolder!!.oldPosition,viewHolder.adapterPosition)
+        onMoved(viewHolder!!.oldPosition, viewHolder.adapterPosition)
     }
 
     override fun isLongPressDragEnabled(): Boolean {
