34bc2be5ad17eb7b093afa8cd2089b17458501c5
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-11-15 12:21:37 +0530

    recycler view adapter implements ItemTouchHelperAdapter
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- app/src/main/java/me/rishabhkhanna/recyclerviewswipedrag/RecyclerAdapter.kt
+++ app/src/main/java/me/rishabhkhanna/recyclerviewswipedrag/RecyclerAdapter.kt
@@ -2,17 +2,39 @@ package me.rishabhkhanna.recyclerviewswipedrag
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
-import java.util.ArrayList
+import me.rishabhkhanna.recyclerswipedrag.ItemTouchHelperAdapter
+import java.util.*
 
 /**
  * Created by rishabhkhanna on 14/11/17.
  */
-class RecyclerAdapter(internal var arrayList: ArrayList<String>, internal var context: Context) : RecyclerView.Adapter<RecyclerAdapter.Holder>() {
+class RecyclerAdapter(internal var arrayList: ArrayList<String>, internal var context: Context) : RecyclerView.Adapter<RecyclerAdapter.Holder>(), ItemTouchHelperAdapter {
+    val TAG = "TopicAdapter"
+    override fun onItemMoved(fromPosition: Int, toPosition: Int) {
+        Log.d(TAG, "onItemMoved: FROM: " + fromPosition)
+        Log.d(TAG, "onItemMoved: TO: " + toPosition)
+        if (fromPosition < toPosition) {
+            for (i in fromPosition until toPosition) {
+                Collections.swap(arrayList, i, i + 1)
+            }
+        } else {
+            for (i in fromPosition downTo toPosition + 1) {
+                Collections.swap(arrayList, i, i - 1)
+            }
+        }
+        notifyItemMoved(fromPosition, toPosition)
+    }
+
+    override fun onMoved(fromPos: Int, toPos: Int) {
+        Log.d(TAG, "onSwipe: FROM: " + fromPos)
+        Log.d(TAG, "onSwipe: TO: " + toPos)
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder {
         val li = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
@@ -28,6 +50,6 @@ class RecyclerAdapter(internal var arrayList: ArrayList<String>, internal var co
     }
 
     inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        val tvName: TextView= itemView.findViewById(R.id.tvName)
+        val tvName: TextView = itemView.findViewById(R.id.tvName) as TextView
     }
 }
