d6f52183989ae2ce129a60b9df173203f74f1a8d
Rishabh Khanna <rishabhkhanna777@gmail.com>
2017-11-15 20:14:38 +0530

    swipe listener interface for recycler item
    Signed-off-by: Rishabh Khanna <rishabhkhanna777@gmail.com>

--- lib/src/main/java/me/rishabhkhanna/recyclerswipedrag/onDragListener.kt
+++ lib/src/main/java/me/rishabhkhanna/recyclerswipedrag/onDragListener.kt
@@ -4,4 +4,5 @@ package me.rishabhkhanna.recyclerswipedrag
  * Created by rishabhkhanna on 15/11/17.
  */
 interface onDragListener {
+    fun onDragItemListener(fromPosition: Int, toPosition: Int)
 }
